921a4369b045e5da79ab6b899b12e001b9518a34
Nikhil26062000 <nikhilsbn06@gmail.com>
2023-12-26 00:39:39 +0530

    Modifications

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import Button from "./Button";
 
 export const Header = () => {
   return (
-    <div className=" w-[90%] mx-auto h-24 mt-4 flex justify-between items-center  sm:bg-yellow-500 md:w-[80%] md:bg-green-600 lg:bg-pink-500 xl:bg-slate-600">
+    <div className=" w-[90%] mx-auto h-24 mt-4 flex justify-between items-center ">
 
       <div className="w-[80px] h-[80px] rounded-full bg-violet-600 flex justify-center items-center text-white ">
         <h2 className="logo text-5xl font-extrabold">N</h2>
