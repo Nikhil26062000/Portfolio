1af398e8a1da477e7c7d24e8d40e7704e96ec0fa
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-02-12 17:36:38 +0530

    Font size issue fixed in header

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Logo = () => {
   return (
     <div>
-        <div className="w-[80px] h-[80px] shadow-sm shadow-white rounded-full bg-violet-600 flex justify-center items-center text-white ">
-        <h2 className="logo text-5xl font-extrabold">N</h2>
+        <div className="w-[80px] h-[80px] shadow-sm shadow-white rounded-full bg-violet-600 flex justify-center items-center text-white max-sm:w-[50px] max-sm:h-[50px] ">
+        <h2 className="logo text-5xl font-extrabold max-sm:text-3xl">N</h2>
       </div>
     </div>
   )
